87a92513621ef03b2d19f895495edd3cfd24183f
AR Crypto <37816162+ALLRiPPED@users.noreply.github.com>
2019-10-25 20:31:09 -0500

    rename var to let

--- website_example/config.js
+++ website_example/config.js
@@ -1,14 +1,14 @@
-var api = "http://192.168.1.135:8118";
+let api = "http://192.168.1.135:8118";
 
-var email = "support@allripped.net";
-var telegram = "";
-var discord = "";
-var facebook = "";
+let email = "support@allripped.net";
+let telegram = "";
+let discord = "";
+let facebook = "";
 
-var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+let marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "http://explorer.mutexcurrency.io:8081/block/{id}";
-var transactionExplorer = "http://explorer.mutexcurrency.io:8081/tx/{id}";
+let blockchainExplorer = "http://explorer.mutexcurrency.io:8081/block/{id}";
+let transactionExplorer = "http://explorer.mutexcurrency.io:8081/tx/{id}";
 
-var themeCss = "themes/default.css";
-var defaultLang = 'en';
+let themeCss = "themes/default.css";
+let defaultLang = 'en';
